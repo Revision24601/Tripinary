262ce8d52b42c5094c5dec91498044c58ebb9d62
Nischay Mohan <spectralnischay@hotmail.com>
2014-06-22 03:04:19 -0400

    City in search field, convert it into lat and long, and display raw data based on the coordinates. I'm sleepy.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,24 @@ require 'google_places'
 class SearchController < ApplicationController
   def index
   	@search_query = params[:query]
+
+  	puts "PARAMS PLS" 
+  	puts params
   	@client = GooglePlaces::Client.new("AIzaSyB8JsJYe-lexJph91XALK-XhPxPQZAlk48")
 
-  	@spots = @client.spots(41.9100711,12.5359979)
+  	# @spots = @client.spots(41.9100711,12.5359979)
+
+  	@placeholdertypesarray = ['museum', 'zoo', 'establishment']
+
+  	@torontospots = @client.spots_by_query(@search_query)
+  	@lat = @torontospots[0].lat
+  	@lng = @torontospots[0].lng
+  	@spots = @client.spots(@lat, @lng, :radius => 40000)
+  	# @sortedspots = @spots.sort! { |a,b| a[:rating] <=> b[:rating] }
+  	# @sortedspots = @spots.sort{|spots,b| b[:rating] <=> spots[:rating]}
+
+  	# @spots = @client.spots(@lat, @lng, :radius => 40000)
+
 
 
 
